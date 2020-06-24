eb84874b1e2748981f5eb58e3a921cd1e1895c57
Tasha Lane <designtmg77@gmail.com>
2020-06-24 18:49:22 -0500

    updates to patients

--- main.js
+++ main.js
@@ -15,14 +15,34 @@ btn.addEventListener("click", function () {
     };
     ourRequest.send();
     pageCounter++;
+    
+    
 });
 
 //function to add HTML to the page, add html to empty div
 function renderHTML(data) {
     var htmlString = "";
 
-    for (i = 0; i < data.length; i++) {
-        htmlString += "<p>" + data[i].name + "has" + data[i].diagnosis + ".</p>";
+    for (i = 0; i <data.length; i++) {
+        htmlString +=
+            "<p>" + "Patient's name is" +
+            data[i].name +
+            "this patient has" +
+            data[i].diagnosis + "as a current diagnosis" +
+            data[i].vitaminDeficiencies + "are the current vitamin deficiencies" +
+            data[i].treatment + "is suggested for treatment" +
+            data[i].symptoms + "are current symptoms";
+    for (ii = 0; ii < data[i].symptoms.allergies.length; ii++){
+        if (ii == 0) {
+            htmlString += data[i].symptoms.allergies[ii];
+        } else {
+            htmlString += "and" + data[i].symptoms.allergies[ii];
+        }
+    }
+        ////Tasha you will have to go back and do the dislikes the same way you did for loop for the likes..
+
+    
+          ".</p>";
     }
 
 
